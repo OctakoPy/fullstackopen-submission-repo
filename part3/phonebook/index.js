524fb881bd28fc010b06ee3e15244025bcae7955
OctakoPy <octakopy@gmail.com>
2025-02-15 12:05:12 +0800

    3.8*: Phonebook backend step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 app.use(express.json());
+
+morgan.token('post-data', (req) => {
+    return req.method === 'POST' ? JSON.stringify(req.body) : '';
+  });
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'));
+
 
 let persons = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
