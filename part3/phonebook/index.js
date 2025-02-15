382a2b07db726faac6b449e8690aa86dba9a6bf8
OctakoPy <octakopy@gmail.com>
2025-02-15 15:32:22 +0800

    Update index.js

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -82,7 +82,8 @@ app.delete('/api/persons/:id', (req, res) => {
     }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
